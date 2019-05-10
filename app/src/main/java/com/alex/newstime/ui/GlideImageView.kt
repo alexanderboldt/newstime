21f4fba5f755362c9aea2298230c965e0404d352
Alexander Boldt <alexx1216@live.de>
2019-05-10 14:09:08 +0200

    added fallback-image when an error occurs

--- app/src/main/java/com/alex/newstime/ui/GlideImageView.kt
+++ app/src/main/java/com/alex/newstime/ui/GlideImageView.kt
@@ -3,18 +3,21 @@ package com.alex.newstime.ui
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageView
+import com.alex.newstime.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 
-class GlideImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null)
-    : ImageView(context, attrs) {
-
-    // ----------------------------------------------------------------------------
+class GlideImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : ImageView(context, attrs) {
 
     fun setImage(url: String?) {
+        val options = RequestOptions()
+            .error(R.drawable.ic_image)
+            .centerCrop()
+
         Glide.with(context)
-                .load(url)
-                .apply(RequestOptions.centerCropTransform())
-                .into(this)
+            .load(url)
+            .apply(options)
+            .into(this)
     }
 }
